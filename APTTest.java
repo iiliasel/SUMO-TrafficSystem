83ef224da20615fb8f99d07943edac49333d4654
iiliasel <fifat26@gmail.com>
2025-11-27 21:57:57 +0100

    Add comment for local SUMO installation paths
    Added a comment to remind users to update paths for SUMO installation.

--- APTTest.java
+++ APTTest.java
@@ -5,6 +5,7 @@ import org.eclipse.sumo.libtraci.StringVector;
 public class APTTest {
     public static void main(String[] args) {
         Simulation.preloadLibraries();
+        // IMPORTANT: Update the paths below to match your local SUMO installation
         Simulation.start(new StringVector(new String[] {"D:\\SUMO\\sumo-1.25.0\\bin\\sumo-gui.exe", "-c", "D:\\SUMO\\demo_practice\\exercise.sumocfg"}));
         for (int i = 0; i < 5; i++) {
             Simulation.step();
